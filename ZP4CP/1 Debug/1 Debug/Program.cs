b30a644bcce9ab68c910d4008ee148e5637caf90
Michael Klunko <32402640+klnmi97@users.noreply.github.com>
2018-02-12 17:34:30 +0100

    first exercise added

--- ZP4CP/1 Debug/1 Debug/Program.cs
+++ ZP4CP/1 Debug/1 Debug/Program.cs
@@ -10,6 +10,8 @@ namespace _1_Debug
     {
         private static void MergeSort(int[] arr, int l, int r)
         {
+//preprocessor function Debug
+//prints string from settings
 #if DEBUG
             Console.WriteLine(Properties.Settings.Default.DEBUG);
 #endif
