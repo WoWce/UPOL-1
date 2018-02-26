5cd7e732652c31a2be8cae15cf59a1edc440a70b
Michael Klunko <32402640+klnmi97@users.noreply.github.com>
2018-02-26 13:04:22 +0100

    exercise 3

---
+++ ZP4CS/3 Parallel/ParallelMergeSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _3_Parallel
+{
+    class ParallelMergeSort
+    {
+        public void PMergeSort(int[] arr, int p, int r, int depth)
+        {
+            
+            depth--;
+            if (p < r && depth > 0)
+            {
+                int q = (p + r) / 2;
+                Thread t1 = new Thread(() => PMergeSort(arr, p, q, depth));
+                Thread t2 = new Thread(() => PMergeSort(arr, q+1, r, depth));
+                t1.Start();
+                t2.Start();
+                t1.Join();
+                t2.Join();
+
+                Merge(arr, p, q, r);
+            }
+            else if(p < r)
+            {
+                int q = (p + r) / 2;
+                SequentialMergeSort(arr, p, q);
+                SequentialMergeSort(arr, q + 1, r);
+                Merge(arr, p, q, r);
+            }
+        }
+
+        private void Merge(int[] A, int p, int q, int r)
+        {
+            int n1 = q - p + 1;
+            int n2 = r - q;
+            int[] L = new int[n1 + 1];
+            int[] R = new int[n2 + 1];
+            Array.Copy(A, p, L, 0, L.Length - 1);
+            Array.Copy(A, q + 1, R, 0, R.Length - 1);
+
+            L[n1] = int.MaxValue;
+            R[n2] = int.MaxValue;
+
+            int n = 0;
+            int m = 0;
+            for (int k = p; k <= r; k++)
+            {
+                if (L[n].CompareTo(R[m]) < 0 || L[n].CompareTo(R[m]) == 0)
+                {
+                    A[k] = L[n];
+                    n++;
+                }
+                else
+                {
+                    A[k] = R[m];
+                    m++;
+                }
+            }
+        }
+
+        private void SequentialMergeSort(int[] arr, int l, int r)
+        {
+
+            if (l < r)
+            {
+                int q = (l + r) / 2;
+                SequentialMergeSort(arr, l, q);
+                SequentialMergeSort(arr, q + 1, r);
+                Merge(arr, l, q, r);
+            }
+        }
+
+    }
+}
---
+++ ZP4CS/3 Parallel/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Parallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = GetRandomArray(1000000);
+            int[] arr2 = new int[arr.Length];
+            int[] arr3 = new int[arr.Length];
+            Array.Copy(arr, arr2, arr.Length);
+            Array.Copy(arr, arr3, arr.Length);
+
+            Stopwatch sw = Stopwatch.StartNew();
+            ParallelMergeSort(arr, 1);
+            sw.Stop();
+            Console.WriteLine("Sequential: {0}ms", sw.Elapsed.TotalMilliseconds);
+            Console.WriteLine(IsSorted(arr));
+
+            sw = Stopwatch.StartNew();
+            ParallelMergeSort(arr2, 2);
+            sw.Stop();
+            Console.WriteLine("Depth = 1: {0}ms", sw.Elapsed.TotalMilliseconds);
+            Console.WriteLine(IsSorted(arr2));
+
+            sw = Stopwatch.StartNew();
+            ParallelMergeSort(arr3, 3);
+            sw.Stop();
+            Console.WriteLine("Depth = 2: {0}ms", sw.Elapsed.TotalMilliseconds);
+            Console.WriteLine(IsSorted(arr3));
+            
+            Console.ReadLine();
+        }
+
+        public static int[] GetRandomArray(int size)
+        {
+            int[] arr = new int[size];
+            Random randNum = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = randNum.Next(0, 1000);
+            }
+            return arr;
+        }
+
+        public static void ParallelMergeSort(int[] arr, int depth)
+        {
+            ParallelMergeSort sort = new ParallelMergeSort();
+            sort.PMergeSort(arr, 0, arr.Length - 1, depth);
+        }
+
+        public static void PrintArray<T>(T[] t)
+        {
+            foreach (T item in t)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+        }
+
+        public static bool IsSorted(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
