abf52a63cb646bca6925828baccdcf3176dbe842
klnmi97 <32402640+klnmi97@users.noreply.github.com>
2018-03-27 11:33:29 +0200

    Slider fixed

--- INFS/scripts.js
+++ INFS/scripts.js
@@ -1,3 +1,4 @@
+/*Scroll up*/
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction(){
@@ -16,4 +17,30 @@ function scrollToTop() {
             }
             else clearInterval(scrollInterval);
         },15);
+}
+
+/*Slider*/
+
+var slideIndex = 0;
+slideShow();
+
+function slideShow() {
+    var i;
+    var articleArr = document.getElementsByClassName("slider-article");
+
+    var tmp;
+    for(i = 0; i<articleArr.length; i++){
+        if(i == 0){
+            tmp = {innerHTML: articleArr[0].innerHTML};
+        }
+        if(i == articleArr.length - 1){
+            articleArr[i].innerHTML = tmp.innerHTML;
+        } else{
+            articleArr[i].innerHTML = articleArr[i+1].innerHTML;
+        }
+    }
+
+    slideIndex++;
+    if (slideIndex > articleArr.length) {slideIndex = 1}
+    setTimeout(slideShow, 9000);
 }
